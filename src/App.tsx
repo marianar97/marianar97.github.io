06b593d331c2a6a8be4b02014ed154ce84cbceef
marianar97 <mariana.ramirezd97@gmail.com>
2025-08-10 14:45:58 -0700

    feat: Add MDX module declaration to support importing .mdx files in Vite

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
 import Projects from "./pages/Projects";
 
 const queryClient = new QueryClient();
@@ -19,6 +20,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
           {/* <Route path="/projects" element={<Projects />} /> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+declare module '*.mdx' {
+  import { ComponentType } from 'react'
+  export const frontmatter: Record<string, any>
+  const MDXComponent: ComponentType<any>
+  export default MDXComponent
+}
